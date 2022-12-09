18d78f2102325c09b6f2c9682ddcf3117245a5c6
radu.micea@student.upt.ro <radu.micea@student.upt.ro>
2022-12-10 00:39:08 +0200

    Get products from response

--- InflationArchiveApi/Services/MegaImageScraper.cs
+++ InflationArchiveApi/Services/MegaImageScraper.cs
@@ -10,13 +10,13 @@ public class MegaImageScraper : AbstractStoreScraper
     private const string RequestUrlBase =
         "https://api.mega-image.ro/?operationName=GetCategoryProductSearch&variables={\"lang\":\"ro\",\"category\":\"<insert_category>\",\"pageNumber\":<insert_page_number>,\"pageSize\":50}&extensions={\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"10ddc63b94cf5c83b7474746ae22bab24e83d503834a72942577672af7df4cb2\"}}";
 
-    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly HttpClient _httpClient = new();
 
     public MegaImageScraper(HttpClient httpClient, ProductService productService) : base(httpClient, productService)
     {
     }
 
-    protected override string StoreName => "MegaImage";
+    protected override string StoreName => "Mega Image";
 
     protected override List<KeyValuePair<string, string[]>> generateRequests()
     {
@@ -33,10 +33,37 @@ public class MegaImageScraper : AbstractStoreScraper
         return result.ToList();
     }
 
-    protected override Task<IEnumerable<Product>> interpretResponse(HttpResponseMessage responseMessage,
+    protected override async Task<IEnumerable<Product>> interpretResponse(HttpResponseMessage responseMessage,
         Category category)
     {
-        throw new NotImplementedException();
+        var products = new List<Product>();
+
+        var responseMessageContent = responseMessage.Content;
+        var json = await responseMessageContent.ReadAsStringAsync();
+        var result = JObject.Parse(json);
+
+        var productTokens = result["data"]!["categoryProductSearch"]!["products"]!.ToList();
+
+        foreach (var token in productTokens)
+        {
+            var manufacturerName = (string)token["manufacturerName"]!;
+            var manufacturerRef = manufacturerReferences.ContainsKey(manufacturerName)
+                ? manufacturerReferences[manufacturerName]
+                : await CreateOrGetManufacturer(manufacturerName);
+
+            products.Add(new Product
+            {
+                Name = (string)token["name"]!,
+                Category = category,
+                Manufacturer = manufacturerRef,
+                Store = storeReference,
+                PricePerUnit = (double)token.SelectToken("price.unitPrice")!,
+                Unit = (string)token.SelectToken("price.unit")!,
+                ImageUri = $"https://d1lqpgkqcok0l.cloudfront.net{(string)token["images"]!.Children().First()["url"]!}"
+            });
+        }
+
+        return products;
     }
 
     private async Task<IEnumerable<string>> BuildRequestForCategory(string category)
