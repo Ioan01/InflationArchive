a4a29031e0f3a73f29e4d5beccf6bcae1c377eca
radu.micea@student.upt.ro <radu.micea@student.upt.ro>
2022-12-09 23:09:48 +0200

    Generated requests for Mega

---
+++ InflationArchiveApi/Services/Helpers/Categories.cs
@@ -0,0 +1,29 @@
+﻿namespace InflationArchive.Services.Helpers;
+
+public static class Categories
+{
+    static Categories()
+    {
+        MegaImageCategories = new Dictionary<string, ISet<string>>
+        {
+            ["Fructe/Legume"] = new HashSet<string> { "001" },
+            ["Lactate/Oua"] = new HashSet<string> { "002" },
+            ["Mezeluri"] = new HashSet<string> { "003001" },
+            ["Carne"] = new HashSet<string> { "003002" },
+            ["Peste si icre"] = new HashSet<string> { "003003" },
+            ["Semipreparate"] = new HashSet<string> { "003004" },
+            ["Produse congelate"] = new HashSet<string> { "004" },
+            ["Cafea/Mic dejun"] = new HashSet<string> { "005001", "005002", "005005", "005006", "005007" },
+            ["Paine/Patiserie"] = new HashSet<string> { "005004" },
+            ["Zahar/Faina/Malai"] = new HashSet<string> { "007001" },
+            ["Orez/Legume uscate"] = new HashSet<string> { "007002" },
+            ["Paste"] = new HashSet<string> { "007004" },
+            ["Condimente"] = new HashSet<string> { "007005" },
+            ["Conserve"] = new HashSet<string> { "007007", "007008", "007009", "007010", "007011" },
+            ["Apa/Sucuri"] = new HashSet<string> { "008" },
+            ["Alcool/Tutun"] = new HashSet<string> { "009" }
+        };
+    }
+
+    public static Dictionary<string, ISet<string>> MegaImageCategories { get; }
+}
---
+++ InflationArchiveApi/Services/MegaImageScraper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Concurrent;
+using InflationArchive.Models.Products;
+using InflationArchive.Services.Helpers;
+using Newtonsoft.Json.Linq;
+
+namespace InflationArchive.Services;
+
+public class MegaImageScraper : AbstractStoreScraper
+{
+    private const string RequestUrlBase =
+        "https://api.mega-image.ro/?operationName=GetCategoryProductSearch&variables={\"lang\":\"ro\",\"category\":\"<insert_category>\",\"pageNumber\":<insert_page_number>,\"pageSize\":50}&extensions={\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"10ddc63b94cf5c83b7474746ae22bab24e83d503834a72942577672af7df4cb2\"}}";
+
+    private readonly HttpClient _httpClient = new HttpClient();
+
+    public MegaImageScraper(HttpClient httpClient, ProductService productService) : base(httpClient, productService)
+    {
+    }
+
+    protected override string StoreName => "MegaImage";
+
+    protected override List<KeyValuePair<string, string[]>> generateRequests()
+    {
+        var result = new ConcurrentBag<KeyValuePair<string, string[]>>();
+
+        Parallel.ForEach(Categories.MegaImageCategories.Keys, key =>
+        {
+            result.Add(new KeyValuePair<string, string[]>
+            (
+                key, BuildRequestForCategory(key).Result.ToArray()
+            ));
+        });
+
+        return result.ToList();
+    }
+
+    protected override Task<IEnumerable<Product>> interpretResponse(HttpResponseMessage responseMessage,
+        Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task<IEnumerable<string>> BuildRequestForCategory(string category)
+    {
+        var result = new ConcurrentBag<string>();
+
+        var codes = Categories.MegaImageCategories[category];
+
+        await Parallel.ForEachAsync(codes, async (c, _) =>
+        {
+            var req = RequestUrlBase
+                .Replace("<insert_category>", c)
+                .Replace("<insert_page_number>", "0");
+
+            var response = await _httpClient.GetAsync(req, _);
+            var json = await response.Content.ReadAsStringAsync(_);
+
+            var jObj = JObject.Parse(json);
+            var nrPages = (int)jObj.SelectToken("data.categoryProductSearch.pagination.totalPages")!;
+
+            for (var i = 0; i < nrPages; i++)
+            {
+                result.Add(RequestUrlBase
+                    .Replace("<insert_category>", c)
+                    .Replace("<insert_page_number>", i.ToString()));
+            }
+        });
+
+        return result;
+    }
+}
