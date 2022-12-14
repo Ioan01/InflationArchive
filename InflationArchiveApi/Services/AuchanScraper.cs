1814a98463d62867f4538d7e2df73f1142afdc4e
sinisconi2002 <simion.inisconi@student.upt.ro>
2022-12-14 09:29:17 +0200

    first

---
+++ InflationArchiveApi/Services/AuchanScraper.cs
@@ -0,0 +1,22 @@
+﻿using InflationArchive.Models.Products;
+
+namespace InflationArchive.Services;
+
+public class AuchanScraper : AbstractStoreScraper
+{
+    private const string RequestUrlBase = "";
+    public AuchanScraper(HttpClient httpClient, ProductService productService) : base(httpClient, productService)
+    {
+    }
+
+    protected override string StoreName => "Auchan";
+    protected override List<KeyValuePair<string, string[]>> generateRequests()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Task<IEnumerable<Product>> interpretResponse(HttpResponseMessage responseMessage, Category category)
+    {
+        throw new NotImplementedException();
+    }
+}
