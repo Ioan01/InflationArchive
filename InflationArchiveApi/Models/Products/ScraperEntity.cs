40cc66c0dd2d24e5b01df3cfde0c35fa54696cb8
radu.micea@student.upt.ro <radu.micea@student.upt.ro>
2022-12-15 01:18:12 +0200

    Fuck my life

--- InflationArchiveApi/Models/Products/ScraperEntity.cs
+++ InflationArchiveApi/Models/Products/ScraperEntity.cs
@@ -2,7 +2,7 @@ namespace InflationArchive.Models.Products;
 
 public class ScraperEntity
 {
-    public int Id { get; init; }
-    public string Name { get; init; }
-    public string? ImageUri { get; init; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? ImageUri { get; set; }
 }
--- InflationArchiveApi/Models/Products/Store.cs
+++ InflationArchiveApi/Models/Products/Store.cs
@@ -1,13 +1,7 @@
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
 namespace InflationArchive.Models.Products;
-
 
 public class Store : ScraperEntity
 {
-    
     public string? Website { get; set; }
-    
     public IEnumerable<Product> Products { get; set; }
 }
