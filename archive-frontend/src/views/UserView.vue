9733ef087331b4ae934f43cc26159a9119b17e77
sinisconi2002 <simion.inisconi@student.upt.ro>
2022-12-18 11:14:10 +0200

    better design

--- archive-frontend/src/views/UserView.vue
+++ archive-frontend/src/views/UserView.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="d-flex-center mt-5">
         <h3 class="text-center mt-5">Details about the following user:</h3>
-        
+        <h3 class="text-center mt-5">User's favorite products:</h3>
+        <v-card class="mx-auto mt-5" max-width="344">
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+
+            <v-card-title>
+                Product name lalala
+            </v-card-title>
+
+            <v-card-subtitle>
+                Do we really need this buddy?
+            </v-card-subtitle>
+            <v-row class="justify-center mx-auto pa-3">
+                <v-btn
+                    color="primary" >
+                    Product details
+                </v-btn>
+            </v-row>
+        </v-card>
     </div>
 </template>
 <script lang="ts">
@@ -17,7 +34,7 @@
         
 
         methods: {
-            
+
         }
     })
 </script>
